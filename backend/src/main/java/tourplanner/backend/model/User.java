af49634971956dbf88b2d22ef35cf0735bcf5dc8
OishikRoyVienna <oishik50@gmail.com>
2026-03-31 23:45:24 +0200

    new files

---
+++ backend/src/main/java/tourplanner/backend/model/User.java
@@ -0,0 +1,31 @@
+package tourplanner.backend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password; // Wird gehasht gespeichert!
+
+    @Column(unique = true)
+    private String email;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Tour> tours = new ArrayList<>();
+}
