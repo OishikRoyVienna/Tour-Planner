67e51abd83e31fbbdf51f2756c73530f3ab62230
Oishik ROY <oishik.roy@siemens.com>
2026-04-28 11:39:29 +0200

    inital backend design

--- backend/TourPlannerApplication.java
+++ backend/TourPlannerApplication.java
@@ -2,9 +2,12 @@ package com.tourplanner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.tourplanner")
 public class TourPlannerApplication {
+
   public static void main(String[] args) {
     SpringApplication.run(TourPlannerApplication.class, args);
   }
